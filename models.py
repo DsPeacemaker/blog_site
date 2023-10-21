e225cdb9cc5bfa040f0e05e8193f4b3637db9a4b
DsPeacemaker <enotikpartizan@mail.ru>
2023-10-21 21:03:03 +0300

    Добавить модель поста

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель данных для постов блога
+        title - заголовок, slug - метка (для БД), body - тело поста"""
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    
+    def __str__(self):
+        return self.title
