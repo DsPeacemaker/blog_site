35e819caf1f6d92307acf90873c334ce48df8343
DsPeacemaker <enotikpartizan@mail.ru>
2023-10-21 21:16:45 +0300

    Добавить полей даты и времени

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -7,6 +8,13 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
     def __str__(self):
         return self.title
