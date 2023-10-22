614763ca0103a25147881621c3503643d6ba57de
DsPeacemaker <enotikpartizan@mail.ru>
2023-10-22 12:50:24 +0300

    Рефракторинг

--- models.py
+++ models.py
@@ -9,16 +9,20 @@ class Post(models.Model):
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
-        PUBLICHED = 'PB', 'Published'
+        PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
