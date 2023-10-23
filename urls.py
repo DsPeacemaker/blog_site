b44a647d617c738f97bbb4a3f47621c9b30a703c
DsPeacemaker <enotikpartizan@mail.ru>
2023-10-23 17:51:34 +0300

    Изменение URL-адреса поста

--- urls.py
+++ urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    #Представление поста
+    # Представление поста
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+         views.post_detail,
+         name='post_detail'),
 ]
