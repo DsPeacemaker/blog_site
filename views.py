3f73c0ae245330662b805ad8eeb48ffd2082592e
DsPeacemaker <enotikpartizan@mail.ru>
2023-11-04 23:59:58 +0300

    миграция на postgres

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
