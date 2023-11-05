809a76e32337a4de3960c6577bada6aad624e1f1
DsPeacemaker <enotikpartizan@mail.ru>
2023-11-05 21:40:09 +0300

    Пооск по траграммному сходству

--- views.py
+++ views.py
@@ -7,6 +7,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 class PostListView(ListView):
@@ -117,12 +118,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body', config='english')
-            search_query = SearchQuery(query, config='english')
+            #search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            #search_query = SearchQuery(query, config='english')
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-                ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query)).filter(similarity__gt=0.1).order_by('similarity')
 
     return render(request,
                   'blog/post/search.html',
